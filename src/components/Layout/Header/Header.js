0bb4e963018a76ffc18db72c38f25f51f080b99f
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-03-12 14:29:05 +0000

    changing colors and shadows box

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -14,6 +14,7 @@ class Header extends Component {
     return (
       <React.Fragment>
         <header>
+
           <div className="flex-wrap d-flex   mb-5">
             <div className="col-sm-12 mt-5   text-center">
               {" "}
@@ -21,10 +22,10 @@ class Header extends Component {
               <h3>&#x3c;100 Days of Code/></h3>
             </div>
 
-            <div className="col-sm-12 mt-3 mb-5 d-flex justify-content-center">
+            <div className="col-sm-12 mt-3  d-flex justify-content-center">
               <img src={personal} alt=" code view" />
             </div>
-            <blockquote className="headerBlockquote text-center mt-5 font-weight-normal  w-100 h1">
+            <blockquote className="headerBlockquote text-center font-weight-normal  w-100 h1">
               {" "}
               <i> Stay passionate and you will thrive, i am sure!</i>
             </blockquote>
