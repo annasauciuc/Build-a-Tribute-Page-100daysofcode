21e3261bebdd9d669c33ec206ec9b6c8d9ecca3c
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-03-25 21:43:43 +0000

    adding new day

--- src/data/days.js
+++ src/data/days.js
@@ -308,6 +308,17 @@ const days = [
     hrefGit: "https://github.com/annasauciuc/basicsAngular.js",
     text: "Angular.js",
     title: "Day 20"
+  },
+  {
+    id: 21,
+    mood: "Study",
+    research: "Starting to read Angular JS by Brad Green & Shyam Seshadri",
+    languages: ["devicon-angularjs-plain colored  fa-2x  mr-1"],
+    date: "25/03/2019",
+    classStyle: "fa-2x devicon-github-plain colored ",
+    hrefGit: "https://github.com/annasauciuc/basicsAngular.js",
+    text: "Angular.js",
+    title: "Day 21"
   }
 ];
 export default days;
